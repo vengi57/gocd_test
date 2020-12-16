541fc19a9e07640dd3f9a3a42080002bf562d718
vengatesan_t <vengatesan.t@bridgei2i.com>
2020-12-16 17:07:58 +0530

    added folder

--- write.py
+++ write.py
@@ -2,4 +2,4 @@ import pandas as pd
 df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
                    'mask': ['red', 'purple'],
                    'weapon': ['sai', 'bo staff']})
-df.to_csv("./file_name.csv",index = False)
+df.to_csv("./output/file_name.csv",index = False)
