b43635ed591c44ac90abc176b8aae41c5a2dc1ca
Vengi Thiyagarajan <32476102+vengi57@users.noreply.github.com>
2020-12-16 17:28:03 +0530

    added three files

--- write.py
+++ write.py
@@ -2,4 +2,6 @@ import pandas as pd
 df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
                    'mask': ['red', 'purple'],
                    'weapon': ['sai', 'bo staff']})
-df.to_csv("./output/file_name.csv",index = False)
+df.to_csv("./output/file_name_1.csv",index = False)
+df.to_csv("./output/file_name_2.csv",index = False)
+df.to_csv("./output/file_name_3.csv",index = False)
